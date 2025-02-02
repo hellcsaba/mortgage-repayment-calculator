5c8e8b033a8d9aaa0f68e9aacee7b50665c90f2c
Csaba Hell <ifj.hellcsaba@gmail.com>
2025-02-02 15:50:09 +0100

    Add BEM notated classnames for App, move assets to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,46 @@ import "./App.css";
 
 function App() {
   return (
-    <main>
-      <form>
-        <div>
-          <h1>Mortgage Calculator</h1>
-          <button type="reset">Clear All</button>
+    <main className="mortgage">
+      <form className="mortgage__form">
+        <div className="mortgage__header">
+          <h1 className="mortgage__title">Mortgage Calculator</h1>
+          <button className="mortgage__reset-button" type="reset">
+            Clear All
+          </button>
         </div>
-        <label>Mortgage Amount</label>
-        <input type="number" />
-        <label>Mortgage Term</label>
-        <input type="number" />
-        <label>Interest Rate</label>
-        <input type="number" />
-        <p>Mortgage Type</p>
-        <label>Repayment</label>
-        <input type="radio" name="mortgage-type" value="repayment" />
-        <label>Interest only</label>
-        <input type="radio" name="mortgage-type" value="interest" />
-        <button type="submit">Calculate Repayments</button>
+
+        <label className="mortgage__label">Mortgage Amount</label>
+        <input className="mortgage__input" type="number" />
+        <label className="mortgage__label">Mortgage Term</label>
+        <input className="mortgage__input" type="number" />
+        <label className="mortgage__label">Interest Rate</label>
+        <input className="mortgage__input" type="number" />
+
+        <fieldset className="mortgage__type">
+          <legend className="mortgage__label">Mortgage Type</legend>
+
+          <label className="mortgage__radio-label">Repayment</label>
+          <input className="mortgage__radio" type="radio" id="repayment" name="mortgage-type" value="repayment" />
+
+          <label className="mortgage__radio-label">Interest only</label>
+          <input className="mortgage__radio" type="radio" id="interest" name="mortgage-type" value="interest" />
+        </fieldset>
+
+        <button className="mortgage__submit-button" type="submit">
+          Calculate Repayments
+        </button>
       </form>
-      <section>
-        <img src="./assets/illustration-empty.svg" alt="Empty results illustration"></img>
-        <p>Results shown here</p>
-        <p>Complete the form and click “calculate repayments” to see what your monthly repayments would be.</p>
+      <section className="mortgage__results">
+        <img
+          className="mortgage__results-image"
+          src="/assets/illustration-empty.svg"
+          alt="Empty results illustration"
+        ></img>
+        <p className="mortgage__results-text">Results shown here</p>
+        <p className="mortgage__results-descrition">
+          Complete the form and click “calculate repayments” to see what your monthly repayments would be.
+        </p>
       </section>
     </main>
   );
